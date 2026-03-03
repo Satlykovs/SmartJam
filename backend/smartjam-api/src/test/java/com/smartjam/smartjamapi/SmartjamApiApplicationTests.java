c8fc5cfeb98c022a41109ce6390f4978bf56661e
Satlykovs <satlykovs@gmail.com>
2026-03-04 00:46:31 +0300

    chore: add comments to disabled tests and update build plugins to latest versions

--- backend/smartjam-api/src/test/java/com/smartjam/smartjamapi/SmartjamApiApplicationTests.java
+++ backend/smartjam-api/src/test/java/com/smartjam/smartjamapi/SmartjamApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+@Disabled("TODO: Enable after configuring Testcontainers for Postgres/Kafka in CI/CD")
 @SpringBootTest
 class SmartjamApiApplicationTests {
 

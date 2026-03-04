9e982dbe6b7e79564889e34957d8a1fbda5019a6
abeb <ABEBE@gmail.com>
2026-03-05 00:21:43 +0300

    chore: add JWT dependencies and enable Lombok

--- backend/smartjam-api/src/main/java/com/smartjam/smartjamapi/dto/RegisterRequest.java
+++ backend/smartjam-api/src/main/java/com/smartjam/smartjamapi/dto/RegisterRequest.java
@@ -3,7 +3,6 @@ package com.smartjam.smartjamapi.dto;
 public record RegisterRequest(
         String username,
         String email,
-        String password,
-        String confirmPassword
+        String password
 ) {
 }

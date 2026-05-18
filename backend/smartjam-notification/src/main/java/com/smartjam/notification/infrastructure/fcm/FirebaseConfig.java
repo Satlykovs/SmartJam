06c9a57360d8150f2da77baffe150f0d4b2ad144
Satlykovs <satlykovs@gmail.com>
2026-05-18 17:46:51 +0300

    feat: verify notification is working via test web app, update .gitignore

--- backend/smartjam-notification/src/main/java/com/smartjam/notification/infrastructure/fcm/FirebaseConfig.java
+++ backend/smartjam-notification/src/main/java/com/smartjam/notification/infrastructure/fcm/FirebaseConfig.java
@@ -9,9 +9,11 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
+@Profile("!debug")
 public class FirebaseConfig {
     @PostConstruct
     public void init() {

b41ea48a80977e4bda88f4e8c6e4f026233df9e4
Satlykovs <satlykovs@gmail.com>
2026-05-19 19:21:47 +0300

    chore: Temporarily disable Spring Boot Test

--- backend/smartjam-notification/src/test/java/com/smartjam/notification/SmartjamNotificationApplicationTests.java
+++ backend/smartjam-notification/src/test/java/com/smartjam/notification/SmartjamNotificationApplicationTests.java
@@ -1,8 +1,10 @@
 package com.smartjam.notification;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("TODO: Enable after configuring Testcontainers for Postgres/Kafka in CI/CD")
 @SpringBootTest
 class SmartjamNotificationApplicationTests {
 
